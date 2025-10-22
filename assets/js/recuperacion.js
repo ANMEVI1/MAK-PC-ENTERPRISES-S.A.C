f7745f31a7919c7569af1c078543838ed14c7c7d
stevenpbodero <javiermeliodas6@gmail.com>
2025-10-22 09:06:59 -0500

    arregle bug del ojito

--- assets/js/recuperacion.js
+++ assets/js/recuperacion.js
@@ -402,24 +402,6 @@ document.addEventListener('DOMContentLoaded', function() {
             // Simular cambio de contraseña exitoso
             simulatePasswordChange(newPassword);
         });
-
-        // Mostrar/ocultar contraseña
-        document.querySelectorAll('.toggle-password').forEach(btn => {
-            btn.addEventListener('click', function() {
-                const input = this.closest('.input-group').querySelector('input');
-                const icon = this.querySelector('i');
-                
-                if (input.type === 'password') {
-                    input.type = 'text';
-                    icon.classList.remove('fa-eye');
-                    icon.classList.add('fa-eye-slash');
-                } else {
-                    input.type = 'password';
-                    icon.classList.remove('fa-eye-slash');
-                    icon.classList.add('fa-eye');
-                }
-            });
-        });
     }
     
     // ==================== MOSTRAR LISTADO DE PREGUNTAS ====================
